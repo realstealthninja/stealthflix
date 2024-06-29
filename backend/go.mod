289636213c987b8d457415896862824677cb9562
realstealthninja <68815218+realstealthninja@users.noreply.github.com>
2024-06-29 21:17:49 +0530

    fix: add more endpoints

--- backend/go.mod
+++ backend/go.mod
@@ -2,16 +2,37 @@ module stealthflix-backend
 
 go 1.22.4
 
+require github.com/labstack/echo/v4 v4.12.0
+
 require (
-	github.com/labstack/echo/v4 v4.12.0 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.4.0 // indirect
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.9.2 // indirect
+	github.com/andybalholm/cascadia v1.3.2 // indirect
+	github.com/antchfx/htmlquery v1.3.2 // indirect
+	github.com/antchfx/xmlquery v1.4.1 // indirect
+	github.com/antchfx/xpath v1.3.1 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible
+	github.com/gobwas/glob v0.2.3 // indirect
+	github.com/gocolly/colly v1.2.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/kennygrant/sanitize v1.2.4 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
-	github.com/t3rm1n4l/go-mega v0.0.0-20240219080617-d494b6a8ace7 // indirect
+	github.com/saintfish/chardet v0.0.0-20230101081208-5e3ef4b5456d // indirect
+	github.com/temoto/robotstxt v1.1.2 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.22.0 // indirect
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	google.golang.org/appengine v1.6.8 // indirect
+	google.golang.org/protobuf v1.26.0 // indirect
 )
--- backend/main.go
+++ backend/main.go
@@ -1,29 +1,83 @@
 package main
 
 import (
+	"log"
 	"net/http"
+	"net/url"
 
+	"stealthflix-backend/cmd"
+
+	"github.com/go-playground/validator"
 	"github.com/labstack/echo/v4"
 )
+
+type MediaValidator struct {
+	validator *validator.Validate
+}
+
+func (mv *MediaValidator) Validate(i interface{}) error {
+	if err := mv.validator.Struct(i); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
+}
 
 func test(ctx echo.Context) error {
 	return ctx.String(http.StatusOK, "<html></html>")
 }
 
-func movie(ctx echo.Context) error {
-	movie_name := ctx.QueryParam("name")
-	return ctx.File("assets/movies/" + movie_name + " ")
+func movies(ctx echo.Context) error {
+
+	movies := cmd.GetMovieList()
+
+	return ctx.JSON(http.StatusOK, movies)
+
+}
+
+func getMovie(ctx echo.Context) error {
+	movieName, err := url.PathUnescape(ctx.Param("name"))
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(movieName)
+	movies := cmd.GetMovies(movieName)
+
+	return ctx.JSON(http.StatusOK, movies)
+}
+
+func moviesPost(ctx echo.Context) error {
+	movie := new(cmd.Media)
+	var err error
+	if err = ctx.Bind(movie); err != nil {
+		return ctx.String(http.StatusBadRequest, "bad request")
+	}
+	if err = ctx.Validate(movie); err != nil {
+		return err
+	}
+
+	sources := cmd.GetSources(*movie)
+
+	return ctx.JSON(http.StatusOK, sources)
+
 }
 
 func main() {
 	e := echo.New()
+	e.Validator = &MediaValidator{validator: validator.New()}
 
 	e.GET("/test", test)
 
-	e.GET("/api/movies/:name", movie)
+	e.GET("/api/movies/", movies)
+	e.POST("/api/movies/", moviesPost)
+
+	e.GET("/api/movies/:name", getMovie)
 
 	// serve angular front end
 	e.Static("/", "static")
+
+	cmd.InitScraper()
+	cmd.ReloadMovies()
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
